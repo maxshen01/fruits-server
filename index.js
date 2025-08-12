bb98ab175892abc58fb12a638de9113f442f6294
maxshen01 <maxshen2503@gmail.com>
2025-08-12 09:30:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//split running of app with the api construction
+
+const { app } = require("./app")
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
---
+++ routes/fruits.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const fruitsRouter = express.Router()
+const fruitsController = require("../constrollers/fruits")
+
+fruitsRouter.get('/', fruitsController.index)
+fruitsRouter.get('/:name', fruitsController.show)
+fruitsRouter.post('/', fruitsController.create)
+fruitsRouter.patch('/:name', fruitsController.update)
+fruitsRouter.delete('/:name', fruitsController.destroy)
+
+module.exports = {fruitsRouter}
